ec45db660c1faede54ba3527fd9511745af10730
Daniel Culveyhouse <dculvey@gmail.com>
2022-01-27 07:39:32 +0000

    Much more work on building the topo grid, mainly in prepare_grid()

--- topographyfloodsim.py
+++ topographyfloodsim.py
@@ -3,25 +3,26 @@
 import sys
 import argparse
 import time
+import random
 
 # Default dimensions if no 2D grid length / width specified
-DEFAULT_LENGTH, DEFAULT_WIDTH, DEFAULT_HEIGHT = 8, 8, 10 
+DEFAULT_LENGTH, DEFAULT_WIDTH, DEFAULT_MAX_HEIGHT = 8, 8, 10 
 # Descriptive key of 3D grid contents
 CONTENT_AIR, CONTENT_BOARD, CONTENT_WATER = 0, 1, 2
 
 parser = argparse.ArgumentParser(description='Just an argparse test.')
-parser.add_argument('-l','--length', metavar='grid length', type=int, nargs='?', default=DEFAULT_LENGTH,
-                    help='length of topography grid')
-parser.add_argument('-w','--width', metavar='grid width', type=int, nargs='?', default=DEFAULT_WIDTH, 
-                    help='width of topography grid')
-parser.add_argument('-g','--grid', dest='area', action='store_true',
-                    help='2D python array, e.g.: [[0,1,0],[1,1,1],[0,2,0]]')
-                    
+parser.add_argument('-l','--length', dest='grid_length', metavar='grid length',
+                    type=int, default=None, help='length of random \
+                    topography grid')
+parser.add_argument('-w','--width', dest='grid_width', metavar='grid width', 
+                    type=int, default=None, help='width of random topography \
+                    grid')
+parser.add_argument('--mh','--max-height', dest='max_height', 
+                    metavar='maximum height', type=int, default=None, 
+                    help='maximum height of random topography grid')
 args = parser.parse_args() 
-if args.area:
-    calc_area = args.length * args.width
-    print(f"The area of the grid is {calc_area}.")
-    
+ 
+
 class MakeCartesianGrid:
     """
     MakeCartesianGrid accepts input of heights for a 2-dimensional grid of squares, then
@@ -46,31 +47,61 @@ class MakeCartesianGrid:
     extrude(self.grid_array)
         Produces the 3D array by extruding the heights of each grid square.
     """      
-    def __init__(self, length, width, height, grid):
-        self.length = length
-        self.width = width
+    def __init__(self, grid, height):
+        self.length = len(grid[0])
+        self.width = len(grid)
         self.height = height
         self.grid = grid        
         
-def prepare_grid(chessboard = None):
+def prepare_grid(topo_grid = None, length = None, width = None, max_height = None):
     """
     prepare_grid accepts a standard Python 2d array input, and normalizes it 
     for extrusion by the MakeCartesianGrid class. It assumes an "x" length 
     based on the length of the first array, then truncates or fills the rest
     of the "y" arrays. It also computes the maximum elevation of the grid's values
+    
+    Alternatively, instead of passing a 2D array as topo_grid, you may create a 
+    random 2D grid of height values by supplying length, width, and maximum height
+    desired. 
 
     Parameters
     ----------
-    chessboard : int, optional
+    topo_grid : int, optional
         A 2d python array, square or rectangular
-
+    topo_grid : int, optional
+        A 2d python array, square or rectangular
     """
-    if chessboard is None: 
+    if topo_grid is None: 
+        # Randomizing a 2D grid
         print("Randomizing a 2D grid")
-        # To be added
-        chessboard = [[0,1,1],[0,3,1],[3,2,2]]
-        
-    print(chessboard)
+        board_length = length or DEFAULT_LENGTH
+        board_width = width or DEFAULT_WIDTH
+        board_max_height = max_height or DEFAULT_MAX_HEIGHT
 
-    return chessboard
+        topo_grid = [[random.randint(0,board_max_height) for i in range(board_length)] for j in range(board_width)]
+
+    else: 
+        # trim rows to first row
+        board_length = len(topo_grid[0])
+        board_width = len(topo_grid)
+        board_max_height = max(map(max, topo_grid))
+        
+        for y in range(len(topo_grid)):
+            if len(topo_grid[y]) < board_length:
+                topo_grid[y].extend([0 for i in range((board_length-len(topo_grid[y])))])  
+            topo_grid[y] = topo_grid[y][0:board_length]
+
+    return topo_grid, board_max_height
     
+def print_grid(topo_grid):
+    for i in range(len(topo_grid)):
+        for j in range(len(topo_grid[0])):
+            #print(topo_grid[i][j], end='|')
+            #print("{:<2}".format(topo_grid[i][j]), end="|")
+            print(str(topo_grid[i][j]).rjust(3), end="|")
+        print("") 
+
+if __name__ == '__main__':
+    topo_grid, board_max_height = prepare_grid(length=args.grid_length, width=args.grid_width, max_height=args.max_height)   
+    print_grid(topo_grid)
+    print(board_max_height)
